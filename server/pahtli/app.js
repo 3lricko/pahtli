4c99ebb0520157e106583771f5a26b243d1f851c
= <=>
2013-07-31 20:45:34 -0700

    Cat example

---
+++ server/pahtli/app.js
@@ -0,0 +1,41 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+, routes = require('./routes')
+, products = require('./routes/admin/products')// ADMIN MODULE
+, http = require('http')
+, path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('productImages', __dirname + '/public/images/products');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.configure('development', function(){
+    app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/admin/products', products.list);
+app.get('/admin/products/update', products.form);
+app.post('/admin/products/update', products.submit(app.get('productImages'))); 
+app.get('/admin/products/delete', products.delete);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+var mongodb = require('mongodb');
+var server = new mongodb.Server('127.0.0.1', 27017, {});
---
+++ server/pahtli/routes/admin/products.js
@@ -0,0 +1,75 @@
+
+/*var products = [];
+
+products.push({
+    
+    name : 'BiOmega',
+    image : 'http://shop.usana.com/media/Shop/2012Rebrand/US/Products/lg-122-US-Biomega.jpg',
+    description : 'Provides advanced and guaranteed levels of EPA and DHA, two long-chain omega-3 fatty acids important for memory and learning,Supports sound cardiovascular, immune, health and joint health'
+});
+
+products.push({
+    
+    name : 'Body Rox',
+    image : 'http://shop.usana.com/media/Shop/2012Rebrand/US/Products/lg-104-US-BodyRox.jpg',
+    description : 'Provides advanced and guaranteed levels of EPA and DHA, two long-chain omega-3 fatty acids important for memory and learning,Supports sound cardiovascular, immune, health and joint health'
+});
+*/
+
+var Product = require('../../models/product');
+var path = require('path');
+var fs = require('fs');
+var join = path.join;
+var url = require('url');
+
+exports.list = function(req, res, next){
+
+    Product.find({}, function(err, products){
+	if(err) return next(err);
+	res.render('products/products', { title : 'Products', products : products });
+    });
+
+};
+
+exports.form = function(req, res){
+
+    res.render('products/productsUpdateForm', { title : 'Product Form' });
+};
+
+exports.submit = function(dir){
+
+    console.log('dir ' + dir);
+    return function(req, res, next){
+	var img = req.files.product.image;
+	var name = req.body.product.name || img.name;
+	var description = req.body.product.description;
+	var path = join(dir, img.name);
+
+	fs.rename(img.path, path, function(err){ 
+
+	    if(err)return next(err);
+
+	    Product.create({
+		name: name,
+			image: '/images/products/' + img.name,
+			description: description },
+			   function(err){
+			       if(err) return next(err);
+			       res.redirect('/'); 
+			   });
+	    });
+    };
+};
+
+exports.delete = function(req, res, next){
+
+    var params = url.parse(req.url, true).query;
+
+    Product.find({'name' : params.name}).remove();
+};
+
+
+    
+
+
+
