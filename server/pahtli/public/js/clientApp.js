6cf87513b567b34c0b83b0d4bb2472560257764e
= <=>
2013-08-04 18:13:53 -0700

    dynamic web prescriptions

--- server/pahtli/public/js/clientApp.js
+++ server/pahtli/public/js/clientApp.js
@@ -5,7 +5,7 @@
 angular.module('pahtliApp', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/admin/products/listView', {templateUrl: '/partials/products/listView.html',   controller: IndexCtrl}).
-      //when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
+      when('/admin/products/listView', {templateUrl: '/partials/products/listView.html',   controller: ProductListCtrl}).
+      when('/admin/products/formView', {templateUrl: '/partials/products/formView.html',   controller: ProductFormCtrl}).
       otherwise({redirectTo: '/admin/products/listView'});
 }]);
--- server/pahtli/public/js/controllers.js
+++ server/pahtli/public/js/controllers.js
@@ -1,53 +1,78 @@
 'use strict'
 
 /*Controllers*/
+
 function ProductListCtrl($scope, $http){
 
+
 	loadJsonProducts($scope, $http);
-
-  	$scope.deleteProduct = function(productName){
-
-  		$http.get('/admin/products/delete?name='+productName).success(function(data) {
-	   			
-    		loadJsonProducts($scope, $http);
-
-  		});
-
-  	}
-}
-
-var loadJsonProducts = function(scope, http){
-
-	http.get('/admin/products/json/').success(function(data) {
-    	scope.products = data;
-    });
-}
-
-function IndexCtrl($scope, $http){
-
-
-	$http.get('/admin/products/json/').success(function(jsonProducts) {
-    			$scope.products = jsonProducts;
-    		});
 	
 	$scope.showProductContent = function(){
 
 		$http.get('/admin/products/listView').success(function(data) {
 
-    		$scope.content = data;
-		
-  		});
+			$scope.content = data;
+
+		});
 	}
 
 	$scope.deleteProduct = function(productName){
 
-  		$http.get('/admin/products/delete?name='+productName).success(function(data) {
-	   			
-    		loadJsonProducts($scope, $http);
+		$http.get('/admin/products/delete?name='+productName).success(function(data) {
 
-  		});
+			loadJsonProducts($scope, $http);
 
-  	}
+		});
+
+	}
+}
+
+function ProductFormCtrl($scope, $http){
+
+	var emptyProduct = {
+		name: 'Name',
+		image: 'image url',
+		description: 'Description',
+		days: [],
+		prescriptions: [
+		{
+			id:0,
+			time: new Date(0,0,0,12,0,0,0),
+			quantity: 0
+		}]
+	};
+
+	$scope.product = emptyProduct;
+
+	$scope.addPrescription = function(){
+
+		var prescriptions =  $scope.product.prescriptions;
+		var id = new Date().valueOf();
+		prescriptions.push({
+			id: id,
+			time: "",
+			quantity:0});
+	}
+
+	$scope.deletePrescription = function(prescriptionId){
+
+		var prescriptions = $scope.product.prescriptions;
+		var filter = prescriptions.filter(function(item){return item.id == prescriptionId;});
+		
+		if(filter == null || filter.length == 0) return;
+		var indexToDelete = prescriptions.indexOf(filter[0]);
+		if(indexToDelete >=0)
+			prescriptions.splice(indexToDelete,1);
+	}
+}
+
+
+/*Utils*/
+var loadJsonProducts = function(scope, http){
+
+	http.get('/admin/products/json/').success(function(data) {
+		scope.products = data;
+	});
 }
 
 
